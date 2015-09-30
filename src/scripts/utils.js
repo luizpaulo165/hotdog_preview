00a3847444617026b8bc5de8b76d766ac0eea281
Luiz Paulo Valença <luizpaulo165@gmail.com>
2015-09-30 14:40:39 -0300

    Ajuste na estrutura do projeto, Nova navegação, Novas opções para as páginas

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,128 +1,141 @@
-$(function(){
-	// Title Page
-	$('title').html($('#list-menu a:first').attr('data-name'));
-	
-	// First link iframe
-	$('iframe').attr
-	({
-		'src': $('#list-menu a:first').attr('href')
-	});
-	
-	// size iframe
-	var resizeFrame = function()
-	{
-		var w = $(window).width(),
+var Application;
+
+Application = {
+	init: function () {
+		this.iframePage();
+		this.menu();
+		this.switchPage();
+		this.callMenu();
+	},
+
+	//IframePage
+	iframePage: function () {
+		// size iframe
+		var resizeFrame = function() {
+			var w = $(window).width(),
 			h = $(window).height();
-			
-		$('iframe').css
-		({
-			'width': w + 'px',
-			'height': h + 'px'
-		});
-	};
-	resizeFrame();
-	$(window).bind(
-	{
-			resize: function()
-			{
+
+			$('iframe').css
+			({
+				'width': w + 'px',
+				'height': h + 'px'
+			});
+		};
+		resizeFrame();
+
+		$(window).bind({
+			resize: function(){
 				resizeFrame();
 			}
-	});
+		});
+	},
 
-	// Link functions
-	
-	// verification hide menu
-	if ($('#list-menu a').length <= 1){
-		$('#wrap-menu').hide();
-	}
-	
-	// Add link active first link
-	$('#list-menu a:first').addClass('active');
-	
-	// Verifications links all
-	$('#list-menu a').each(function()
-	{
-		var self = $(this),
-			selfName = self.data('name');
-		
-		// name length
-		if (selfName.length > 8)
-		{
-			namePage = selfName.substring(0,8) + ' ...'
-		}else
-		{
-			namePage = selfName;
-		}
-		
-		// Text link
-		for (var i = 0; i < selfName.length; i++) {
-			self.html(selfName[0])
-		}
-		
-		// Create span
-		$('<span />',{
-			'html': namePage
-		}).appendTo(self);
-		
-	});
+	// Menu
+	menu: function () {
+		// Wrap Menu
+		var divWrapLinks = $('<div />', {
+			'id': 'wrapLinks'
+		}).appendTo($('body'));
+		var divWrapLinks = $('<div />', {
+			'id': 'linksGroups',
+			'html': '<div class="wrapListLinksPages"></div>'
+		}).appendTo($('#wrapLinks'));
 
-	// hover menu
-	var hMenu = $('#list-menu').outerHeight();
-	
-	//position menu
-	$('#list-menu').css
-	({
-		'top':'-'+ hMenu +'px'
-	});
-	
-	$('#wrap-menu').bind
-	({
-		mouseenter: function()
-		{
-			$('#bt-menu').addClass('active');
-			$('#list-menu').css
-			({
-				'visibility':'visible'
+
+		//  create links
+		$.getJSON( "menu/menu.json", function( data ){
+			var items = [],
+			firstPage = data[0].page;
+
+			// First link iframe
+			$('iframe').attr({
+				'src': 'view/' + firstPage + '.html'
 			});
-			$('#list-menu a').each(function(e)
-			{
-				$(this).stop(false,true).animate
-				({
-					'opacity':'1'
-				}, 200 * e);
-			});
-			[].reverse.call($('#list-menu a')).appendTo('#list-menu');
-		},
-		mouseleave: function()
-		{
-			$('#bt-menu').removeClass('active');
-			$('#list-menu').css
-			({
-				'visibility':'hidden'
-			});
-			$('#list-menu a').each(function(e)
-			{
-				$(this).stop(false,true).animate
-				({
-					'opacity':'0'
-				}, 200 * e);
-			});
-			[].reverse.call($('#list-menu a')).appendTo('#list-menu');
-		}
-	});
-	
-	// hover link menu
-	$('#list-menu a').bind
-	({
-		click: function()
-		{
-			var self = $(this);
-			
-			$('#list-menu a').removeClass('active');
+
+			$.each( data, function( val, item ){
+				var titleLink = item.title,
+				linkPage = item.page,
+				descPage = item.description,
+				namePage = "",
+				linkName = "";
+
+		    // Text link
+		    for (var i = 0; i < linkPage.length; i++) {
+		    	linkName = linkPage[0];
+		    }
+
+		    // name length
+		    if (titleLink.length > 8){
+		    	namePage = linkPage.substring(0,8) + ' ...'
+		    }else{
+		    	namePage = linkPage;
+		    }
+
+		    // create element list
+		    var linksElem = $('<div />',{
+		    	'class': 'listLinks',
+		    	'data-link': linkPage,
+		    	'html':'<span class="idPage">'+ linkName +'</span> <span class="titlePage">'+ titleLink +'</span> <p>'+ descPage +'</p> <span class="bullet"></span>'
+		    });
+
+		    // create links
+		    items.push( linksElem );
+
+		  });
+
+		  // Print links html
+		  $('#linksGroups .wrapListLinksPages').html(items);
+		  $('#list-menu').find('a:first').addClass('active');
+
+		  // active first element list
+			$('.listLinks:first').addClass('active');
+
+			// Title Page First Page
+			$('title').html($('.titlePage:first').text());
+		});
+},
+
+	// SwitchPage
+	switchPage: function () {
+		$(document).on('click','.listLinks', function (){
+			var self = $(this),
+			selfLink = self.data('link'),
+			selfTitle = self.find('.titlePage');
+
+			// active page
+			$('.listLinks').removeClass('active');
 			self.addClass('active');
-			
-			// title page
-			$('title').html(self.attr('data-name'));
-		},
-	});
+
+			// First link iframe
+			$('iframe').attr({
+				'src': 'view/'+ selfLink +'.html'
+			});
+
+			// Title Page First Page
+			$('title').html(selfTitle.text());
+
+			// Close menu
+			$('#wrapLinks, #wrap-menu, #wrap-menu #bt-menu').removeClass('active');
+		});
+	},
+
+	// CallMenu
+	callMenu: function (){
+		$('#wrap-menu').click(function (){
+			if ($('#wrapLinks').hasClass('active')) {
+				$('#wrapLinks').removeClass('active');
+				$(this).removeClass('active');
+				$(this).find('#bt-menu').removeClass('active');
+			}else{
+				$('#wrapLinks').addClass('active');
+				$(this).addClass('active');
+				$(this).find('#bt-menu').addClass('active');
+			}
+		});
+	}
+
+}
+// End App
+$(document).ready(function (){
+	Application.init();
 });
